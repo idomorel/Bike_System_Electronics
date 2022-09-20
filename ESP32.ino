b9711a1bfabb565ea36bd49284d15a6de4a632bc
Ido Morel <ido.morel@gmail.com>
2022-09-20 11:57:48 +0300

    Working LCD

--- ESP32.ino
+++ ESP32.ino
@@ -9,7 +9,7 @@ int velocity;
 unsigned long pevtime;
 
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
 {
